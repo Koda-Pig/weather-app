3cd88be7b63548f05d7ca202fb4360828f829772
Josh Koter <josh.koter@gmail.com>
2023-05-07 11:07:38 +0200

    Another attemp

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-export default {
-  build: {
-    env: {
-      VITE_WEATHER_API_KEY: process.env.VITE_WEATHER_API_KEY
+import { defineConfig, loadEnv } from 'vite'
+
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    define: {
+      VITE_WEATHER_API_KEY: env.VITE_WEATHER_API_KEY
     }
   }
-}
+})
